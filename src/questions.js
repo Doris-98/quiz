2011ffd45cc2514772e95222ea1735105b504ffe
Doris-98 <64284464+Doris-98@users.noreply.github.com>
2024-09-24 08:59:24 +0200

    Add files via upload

---
+++ src/questions.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: 'q1',
+    text: 'What is React.js?',
+    answers: [
+      'A library for building user interfaces using declarative code.',
+      'A framework for backend application development.',
+      'A programming language for designing user interfaces.',
+      'A software for editing images and videos.',
+    ],
+  },
+  {
+    id: 'q2',
+    text: 'What is the purpose of React Hooks?',
+    answers: [
+      'They allow using state and other React features in functional components.',
+      'They enable creating server-side applications in React.',
+      'A controller for creating databases in applications.',
+      'They are part of a CSS library for styling components.',
+    ],
+  },
+  {
+    id: 'q3',
+    text: 'What is JSX in React?',
+    answers: [
+      'A JavaScript extension that allows writing HTML inside JavaScript.',
+      'A library for managing state in React applications.',
+      'A tool for generating PDF documents in applications.',
+      'A special version of CSS created for React.',
+    ],
+  },
+  {
+    id: 'q4',
+    text: 'What is the most common way to create a component in React?',
+    answers: [
+      'By defining a JavaScript function that returns JSX (renderable value).',
+      'By creating a new HTML file in the project.',
+      'By using XML to define the component.',
+      'By using SQL to manipulate data in the component.',
+    ],
+  },
+  {
+    id: 'q5',
+    text: 'What does the term "React state" mean?',
+    answers: [
+      'It is an object in a component that holds values and allows the component to re-render when it changes.',
+      'It is a phase in the React component lifecycle.',
+      ' It is a function that renders HTML code directly on the server.',
+      'It is a tool for creating styles in applications.',
+    ],
+  },
+  {
+    id: 'q6',
+    text: 'How do you typically render a list of data in a React application?',
+    answers: [
+      'By using the map() method to iterate over an array of data and return JSX.',
+      'By using a for() loop to iterate over an array of data without returning JSX.',
+      'By using SQL queries to generate HTML.',
+      'By using a while() loop to display components without returning JSX.',
+    ],
+  },
+  {
+    id: 'q7',
+    text: 'Which method cannot be used for conditional rendering in React?',
+    answers: [
+      'Using the #if syntax in JSX.',
+      'Using a ternary operator in JSX.',
+      'Using the && operator in JSX.',
+      'Using an if-else statement inside a functional component.',
+    ],
+  },
+];
